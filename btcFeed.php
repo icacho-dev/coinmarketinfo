c5e1e54650658d80176fb8b3c8ffaec3283d070a
Isaac Cacho <isaac_cacho@swfideas.com>
2016-06-28 14:01:28 -0500

    fix - php headers

--- btcFeed.php
+++ btcFeed.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: http://www.coinmarketinfo.com');
 header('Access-Control-Allow-Origin: http://coinmarketinfo.com');
 include('GoogleNews.php');
 $news = new GoogleNews();
