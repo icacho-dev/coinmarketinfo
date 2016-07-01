580ee4bdc3a1ee0fb0dd187b5e94a9b89108247e
Isaac Cacho <isaac_cacho@swfideas.com>
2016-06-30 21:50:49 -0500

    update - dashboard 1.1.0

--- cm-admin/services/Rest.inc.php
+++ cm-admin/services/Rest.inc.php
@@ -1,42 +1,42 @@
 <?php
 	class REST {
-		
+
 		public $_allow = array();
 		public $_content_type = "application/json";
 		public $_request = array();
-		
-		private $_method = "";		
+
+		private $_method = "";
 		private $_code = 200;
-		
+
 		public function __construct(){
 			$this->inputs();
 		}
-		
+
 		public function get_referer(){
 			return $_SERVER['HTTP_REFERER'];
 		}
-		
+
 		public function response($data,$status){
 			$this->_code = ($status)?$status:200;
 			$this->set_headers();
 			echo $data;
 			exit;
 		}
-		// For a list of http codes checkout http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+		// For a list of http codes checkout: http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 		private function get_status_message(){
 			$status = array(
 						200 => 'OK',
-						201 => 'Created',  
-						204 => 'No Content',  
-						404 => 'Not Found',  
+						201 => 'Created',
+						204 => 'No Content',
+						404 => 'Not Found',
 						406 => 'Not Acceptable');
 			return ($status[$this->_code])?$status[$this->_code]:$status[500];
 		}
-		
+
 		public function get_request_method(){
 			return $_SERVER['REQUEST_METHOD'];
 		}
-		
+
 		private function inputs(){
 			switch($this->get_request_method()){
 				case "POST":
@@ -54,8 +54,8 @@
 					$this->response('',406);
 					break;
 			}
-		}		
-		
+		}
+
 		private function cleanInputs($data){
 			$clean_input = array();
 			if(is_array($data)){
@@ -70,11 +70,11 @@
 				$clean_input = trim($data);
 			}
 			return $clean_input;
-		}		
-		
+		}
+
 		private function set_headers(){
 			header("HTTP/1.1 ".$this->_code." ".$this->get_status_message());
 			header("Content-Type:".$this->_content_type);
 		}
-	}	
+	}
 ?>
